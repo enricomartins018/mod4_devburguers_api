8cde05edffa501aee4ddcfdb2880fa496eef4ac4
enricomartins018 <enrico.martins018@gmail.com>
2022-07-31 01:34:57 -0300

    feat: Conexão com o banco de dados adicionado.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@
 import mongoose from 'mongoose';
 import nodemon from 'nodemon';
 import express from 'express';
+
+const DB_USER = "devburguers_squad4"
+const DB_PASSWORD = encodeURIComponent('3ZK2eMFjSio57Lp3')
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@devburguers.c3dpllu.mongodb.net/?retryWrites=true&w=majority`)
+    // Se der tudo ok com a conexão, o then aparece com a seguinte mensagem:
+    .then(() => {
+        app.listen(3000)
+        console.log("Banco de dados conectado com sucesso!")
+    })
+
+    // Se der erro, o catch aparece com a seguinte mensagem:
+    .catch((err) => console.log(`${err}: Erro de conexão com o banco de dados.`))
