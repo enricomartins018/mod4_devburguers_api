7e334208c7b7c11d8815bbe4639a94ce4db50da0
enricomartins018 <enrico.martins018@gmail.com>
2022-08-01 17:17:43 -0300

    feat: Conexão com o banco de dados efetuada com sucesso.

--- src/index.js
+++ src/index.js
@@ -4,16 +4,7 @@ import nodemon from 'nodemon';
 import express from 'express';
 import "dotenv/config";
 
-const devburguers_db_user = process.env.DB_USER
-const devburguers_db_password = encodeURIComponent(process.env.DB_PASSWORD)
+const app = express()
 
-// Conectando com o banco de dados 
-mongoose.connect(`mongodb+srv://${devburguers_db_user}:${devburguers_db_password}@devburguers.c3dpllu.mongodb.net/?retryWrites=true&w=majority`)
-    // Se der tudo ok com a conexão, o then aparece com a seguinte mensagem:
-    .then(() => {
-        app.listen(3000)
-        console.log("Banco de dados conectado com sucesso!")
-    })
 
-    // Se der erro, o catch aparece com a seguinte mensagem:
-    .catch((err) => console.log(`${err}: Erro de conexão com o banco de dados.`))
+export default app
---
+++ src/server.js
@@ -0,0 +1,16 @@
+import app from './index.js'
+import mongoose from 'mongoose'
+
+const devburguers_db_user = process.env.devburguers_db_user
+const devburguers_db_password = encodeURIComponent(process.env.devburguers_db_password)
+
+// Conectando com o banco de dados 
+mongoose.connect(`mongodb+srv://${devburguers_db_user}:${devburguers_db_password}@devburguers.c3dpllu.mongodb.net/?retryWrites=true&w=majority`)
+    // Se der tudo ok com a conexão, o then aparece com a seguinte mensagem:
+    .then(() => {
+        app.listen(3000)
+        console.log("Banco de dados conectado com sucesso!")
+    })
+
+    // Se der erro, o catch aparece com a seguinte mensagem:
+    .catch((err) => console.log(`${err}: Erro de conexão com o banco de dados.`))
