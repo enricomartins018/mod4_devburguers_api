0c14f90a84a648238672336f769a8b0e84221507
enricomartins018 <enrico.martins018@gmail.com>
2022-08-02 16:45:09 -0300

    fix: const do express realocada para a linha 3.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 // Importações de bibliotecas Express e Dotenv
 import express from 'express';
+const app = express()
 import "dotenv/config";
 
 // rotas da API
 import pedidosRoutes from './routes/pedidosRoutes.js'
 app.use('/pedidos', pedidosRoutes)
-
-// Variável do Express
-const app = express()
 
 // Exportando o Express
 export default app
