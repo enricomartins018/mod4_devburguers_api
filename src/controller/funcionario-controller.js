69623e2dbc59ee4a41758b2931b6a990a900b7bf
Matheus Moura <matheusmouraa@icloud.com>
2022-08-03 02:49:02 -0300

    feat: Adição da rota POST/CREATE

--- src/controller/funcionario-controller.js
+++ src/controller/funcionario-controller.js
@@ -1,2 +1,31 @@
 import express from "express";
 const router = express.Router();
+
+import Funcionario from "../model/funcionario.js";
+
+// CREATE
+router.post("/", async (req, res) => {
+  //req.body
+  const { nome, cargo, email, CPF } = req.body;
+
+  if (!nome) {
+    res.status(422).json({
+      erro: "Funcionário não inserido pois os campos obrigatórios não foram preenchidos",
+    });
+    return;
+  }
+  const funcionario = {
+    nome,
+    cargo,
+    email,
+    CPF,
+  };
+
+  try {
+    // criando dados
+    await Funcionario.create(funcionario);
+    res.status(201).json({ mensagem: "Funcionário inserido com sucesso!" });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
