e4989c7640f3aa36768f61452fa4e4caa4ed3c14
Jiulie Vitória <102330429+JiulieVitoria@users.noreply.github.com>
2022-08-04 18:05:54 -0300

    feat: adicionando create dos fornecedores

--- src/controller/fornecedores-controller.js
+++ src/controller/fornecedores-controller.js
@@ -2,3 +2,35 @@ import express from "express";
 const router = express.Router();
 import Fornecedores from "../model/fornecedores.js";
 
+
+// Create - Criação de dados
+router.post("/", async (req, res) => {
+    // req.body
+    const { nome, cnpj, inscricaoestadual, email, endereco, telefone } = req.body;
+  
+    if (!nome) {
+      res.status(422).json({ error: "O nome é obrigatório!" });
+    }
+  
+    const fornecedores = {
+      nome,
+      cnpj,
+      inscricaoestadual,
+      email,
+      endereco,
+      telefone,
+    };
+    // se der tudo ok, o try aparece
+    try {
+      // criando dados
+      await Fornecedores.create(fornecedores);
+  
+      res.status(201).json({ message: "Fornecedor criado com sucesso!" });
+      //se der erro, o catch aparece
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Tivemos problema em adicionar o novo fornecedor." });
+    }
+  });
+  
