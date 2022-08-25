a037f4cefd16a1f84251d3349ff0149e470d0d13
enricomartins018 <enrico.martins018@gmail.com>
2022-08-25 19:14:34 -0300

    feat: Test route

--- src/server.js
+++ src/server.js
@@ -6,10 +6,9 @@ import mongoose from 'mongoose'
 const devburguers_db_user = process.env.devburguers_db_user
 const devburguers_db_password = encodeURIComponent(process.env.devburguers_db_password)
 const devburguers_db_name = process.env.devburguers_db_name
-const devburguers_db_key = process.env.devburguers_db_key
 
 // Conectando com o banco de dados 
-mongoose.connect(`mongodb+srv://${devburguers_db_user}:${devburguers_db_password}@${devburguers_db_name}.${devburguers_db_key}.mongodb.net/?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://${devburguers_db_user}:${devburguers_db_password}@${devburguers_db_name}.c3dpllu.mongodb.net/?retryWrites=true&w=majority`)
     // Se der tudo ok com a conexão, o then aparece com a seguinte mensagem:
     .then(() => {
         app.listen(process.env.PORT || 3000, ()=>{
@@ -17,6 +16,5 @@ mongoose.connect(`mongodb+srv://${devburguers_db_user}:${devburguers_db_password
         })
        
     })
-
     // Se der erro, o catch aparece com a seguinte mensagem:
     .catch((err) => console.log(`${err}: Erro de conexão com o banco de dados!`))
