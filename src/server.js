b7d2b28bb612c259c18d6921e2e45e2c7e78dc32
enricomartins018 <enrico.martins018@gmail.com>
2022-08-25 19:24:31 -0300

    feat: Server enabled

--- src/server.js
+++ src/server.js
@@ -3,16 +3,15 @@ import app from './index.js'
 //Importando o mongoose
 import mongoose from 'mongoose'
 
-const devburguers_db_user = process.env.devburguers_db_user
-const devburguers_db_password = encodeURIComponent(process.env.devburguers_db_password)
-const devburguers_db_name = process.env.devburguers_db_name
-const devburguers_db_port = 3000
+// const devburguers_db_user = process.env.devburguers_db_user
+// const devburguers_db_password = encodeURIComponent(process.env.devburguers_db_password)
+// const devburguers_db_name = process.env.devburguers_db_name
 
 // Conectando com o banco de dados 
-mongoose.connect(`mongodb+srv://${devburguers_db_user}:${devburguers_db_password}@${devburguers_db_name}.c3dpllu.mongodb.net/?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://devburguers_squad4:3ZK2eMFjSio57Lp3@devburguers.c3dpllu.mongodb.net/?retryWrites=true&w=majority`)
     // Se der tudo ok com a conexão, o then aparece com a seguinte mensagem:
     .then(() => {
-        app.listen(process.env.PORT || `${devburguers_db_port}`, () => {
+        app.listen(process.env.PORT || 3000, () => {
             console.log("Banco de dados conectado com sucesso! \nEndpoint: http://localhost:3000")
         })
 
